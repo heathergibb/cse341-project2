95be8acf1311f599f383adf9ac0ce8aa17c706c4
heathergibb <hrgibb@gmail.com>
2024-11-26 13:04:48 -0700

    testing swagger autogen

--- swagger.js
+++ swagger.js
@@ -1,16 +1,16 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-// const isProduction =process.env.NODE_ENV === 'production';
+const isProduction =process.env.NODE_ENV === 'production';
 
 const doc = {
     info: {
         title: 'Board Games API',
         description: 'List of Board Games and Players'
-    }
+    },
     // host: 'cse341-project2-psq1.onrender.com',
     // schemes: ['https']
-    // host: isProduction ? 'cse341-project2-psq1.onrender.com' : 'localhost:5500',
-    // schemes: isProduction ? ['https'] : ['http']
+    host: isProduction ? 'cse341-project2-psq1.onrender.com' : 'localhost:5500',
+    schemes: isProduction ? ['https'] : ['http']
 };
 
 const outputFile = './swagger.json';
